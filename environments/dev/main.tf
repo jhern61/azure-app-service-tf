e30eabc891c72f47f68f780c2ea53d5b10179fb1
Joe Hernandez <jhern61@gmail.com>
2025-01-24 18:05:12 -0600

    use tvars file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,163 +1,79 @@
-
-
 locals {
-  environment = "dev"
-  location    = "eastus2"
-  tags = {
-    Environment = local.environment
+  environment = var.environment
+  location    = var.location
+  tags = merge(var.tags, {
+    Environment = var.environment
     Terraform   = "true"
-  }
+  })
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-appservice-${local.environment}"
-  location = local.location
+  name     = var.resource_group_name
+  location = var.location
   tags     = local.tags
 }
 
 module "vnet" {
   source = "../../modules/vnet"
 
-  resource_group_name = azurerm_resource_group.rg.name
-  location           = local.location
-  environment        = local.environment
-  vnet_name          = "vnet-appservice-${local.environment}"
-  vnet_address_space = ["10.0.0.0/16"]
-
-  subnet_configurations = {
-    "snet-app-gateway" = {
-      address_prefixes = ["10.0.0.0/24"]
-    }
-    "snet-app-service" = {
-      address_prefixes                               = ["10.0.1.0/24"]
-      delegation                                     = { "delegation" = ["Microsoft.Web/serverFarms"] }
-      private_endpoint_network_policies_enabled     = true
-      service_endpoints                             = ["Microsoft.Web"]
-    }
-    "snet-private-endpoints" = {
-      address_prefixes                               = ["10.0.2.0/24"]
-      private_endpoint_network_policies_enabled     = true
-    }
-  }
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.location
+  environment           = var.environment
+  vnet_name             = var.vnet_name
+  vnet_address_space    = var.vnet_address_space
+  subnet_configurations = var.subnet_configurations
 }
+
 module "sql_server" {
   source = "../../modules/sql-server"
 
-  resource_group_name  = azurerm_resource_group.rg.name
-  location            = local.location
-  environment         = local.environment
-  server_name         = "sql-appservice-${local.environment}"
-  administrator_login = "sqladmin"
-  # This should be stored in a secure location and passed as a variable
-  administrator_login_password = "P@ssw0rd1234!"
-  subnet_id           = module.vnet.subnet_ids["snet-private-endpoints"]
-  allowed_subnet_ids  = [module.vnet.subnet_ids["snet-app-service"]]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  environment         = var.environment
+  server_name         = var.sql_server_name
+  administrator_login = var.sql_administrator_login
 
-  databases = {
-    "db1" = {
-      name        = "appdb1"
-      sku_name    = "S0"
-      max_size_gb = 32
-    }
-  }
+  administrator_login_password = var.sql_admin_password
+  subnet_id                    = module.vnet.subnet_ids["snet-private-endpoints"]
+  allowed_subnet_ids           = [module.vnet.subnet_ids["snet-app-service"]]
+
+  databases = var.databases
 }
 
 module "keyvault" {
   source = "../../modules/keyvault"
 
-  key_vaults = {
-    kv1 = {
-      location              = "East US"
-      resource_group_name   = "rg1"
-      tenant_id             = "tenant-id"
-      private_dns_zone_name = "privatelink.vaultcore.azure.net"
-      subnet_id             = "/subscriptions/xxx/resourceGroups/xxx/providers/Microsoft.Network/virtualNetworks/vnet/subnets/subnet1"
-      environment           = "dev"
-      product               = "xyz"
-    }
-    kv2 = {
-      location              = "West Europe"
-      resource_group_name   = "rg2"
-      tenant_id             = "tenant-id"
-      private_dns_zone_name = "privatelink.vaultcore.azure.net"
-      subnet_id             = "/subscriptions/xxx/resourceGroups/xxx/providers/Microsoft.Network/virtualNetworks/vnet/subnets/subnet2"
-      environment           = "prod"
-      product               = "abc"
-    }
-  }
+  key_vaults = var.key_vaults
 }
 
 module "nat_gateway" {
   source = "../../modules/nat-gateway"
 
   resource_group_name = azurerm_resource_group.rg.name
-  location           = local.location
+  location            = var.location
 
-  nat_gateways = {
-    "main" = {
-      name = "ng-${local.environment}-${local.location}-01"
-      subnet_ids = [
-        module.vnet.subnet_ids["snet-app-service"],
-        module.vnet.subnet_ids["snet-private-endpoints"]
-      ]
-      idle_timeout_in_minutes = 10
-      zones                  = ["1", "2", "3"]
-      tags                   = local.tags
-    }
-  }
+  nat_gateways = var.nat_gateways
 }
 
 module "app_gateway" {
   source = "../../modules/app-gateway"
 
   resource_group_name = azurerm_resource_group.rg.name
-  location           = local.location
-  environment        = local.environment
-  
-  app_gateway_name = "agw-appservice-${local.environment}"
+  location            = var.location
+  environment         = var.environment
+
+  app_gateway_name = var.app_gateway_name
   subnet_id        = module.vnet.subnet_ids["snet-app-gateway"]
 
   # These should be stored in Key Vault and retrieved
-  ssl_certificate_name     = "example-cert"
-  ssl_certificate_data     = filebase64("path/to/certificate.pfx")
-  ssl_certificate_password = "certpassword"
+  ssl_certificate_name     = var.ssl_certificate_name
+  ssl_certificate_data     = var.ssl_certificate_data
+  ssl_certificate_password = var.ssl_certificate_password
 
-  backend_address_pools = {
-    "app1-pool" = {
-      name  = "app1-pool"
-      fqdns = [module.app_service.app_service_default_hostnames["app1"]]
-    }
-  }
-
-  backend_http_settings = {
-    "app1-https" = {
-      name                  = "app1-https"
-      cookie_based_affinity = "Disabled"
-      port                 = 443
-      protocol             = "Https"
-      request_timeout      = 30
-      host_name            = module.app_service.app_service_default_hostnames["app1"]
-    }
-  }
-
-  http_listeners = {
-    "app1-listener" = {
-      name       = "app1-listener"
-      host_name  = "app1.example.com"
-      require_sni = true
-    }
-  }
-
-  request_routing_rules = {
-    "app1-rule" = {
-      name                       = "app1-rule"
-      rule_type                 = "Basic"
-      http_listener_name        = "app1-listener"
-      backend_address_pool_name = "app1-pool"
-      backend_http_settings_name = "app1-https"
-      priority                  = 100
-    }
-  }
+  backend_address_pools = var.backend_address_pools
+  backend_http_settings = var.backend_http_settings
+  http_listeners        = var.http_listeners
+  request_routing_rules = var.request_routing_rules
 }
 
 data "azurerm_client_config" "current" {}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -32,3 +32,134 @@ variable "app_gateway_domain" {
   type        = string
   default     = "example.com"
 }
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the Virtual Network"
+  type        = list(string)
+}
+
+variable "subnet_configurations" {
+  description = "Map of subnet configurations"
+  type = map(object({
+    address_prefixes                           = list(string)
+    delegation                                 = optional(map(list(string)))
+    private_endpoint_network_policies_enabled = optional(bool)
+    service_endpoints                         = optional(list(string))
+  }))
+}
+
+variable "sql_server_name" {
+  description = "Name of the SQL Server"
+  type        = string
+}
+
+variable "sql_administrator_login" {
+  description = "SQL Server administrator login"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "databases" {
+  description = "Map of database configurations"
+  type = map(object({
+    name        = string
+    sku_name    = string
+    max_size_gb = number
+  }))
+}
+
+variable "key_vaults" {
+  description = "Map of key vault configurations"
+  type = map(object({
+    location              = string
+    resource_group_name   = string
+    tenant_id             = string
+    private_dns_zone_name = string
+    subnet_id             = string
+    environment          = string
+    product             = string
+  }))
+}
+
+variable "nat_gateways" {
+  description = "Map of NAT gateway configurations"
+  type = map(object({
+    name                    = string
+    subnet_ids             = list(string)
+    idle_timeout_in_minutes = number
+    zones                  = list(string)
+    tags                   = map(string)
+  }))
+}
+
+variable "app_gateway_name" {
+  description = "Name of the Application Gateway"
+  type        = string
+}
+
+variable "ssl_certificate_name" {
+  description = "Name of the SSL certificate"
+  type        = string
+}
+
+variable "ssl_certificate_data" {
+  description = "Base64-encoded SSL certificate data"
+  type        = string
+  sensitive   = true
+}
+
+variable "backend_address_pools" {
+  description = "Map of backend address pool configurations"
+  type = map(object({
+    name  = string
+    fqdns = list(string)
+  }))
+}
+
+variable "backend_http_settings" {
+  description = "Map of backend HTTP settings configurations"
+  type = map(object({
+    name                  = string
+    cookie_based_affinity = string
+    port                 = number
+    protocol             = string
+    request_timeout      = number
+    host_name            = string
+  }))
+}
+
+variable "http_listeners" {
+  description = "Map of HTTP listener configurations"
+  type = map(object({
+    name        = string
+    host_name   = string
+    require_sni = bool
+  }))
+}
+
+variable "request_routing_rules" {
+  description = "Map of request routing rule configurations"
+  type = map(object({
+    name                       = string
+    rule_type                 = string
+    http_listener_name        = string
+    backend_address_pool_name = string
+    backend_http_settings_name = string
+    priority                  = number
+  }))
+}
